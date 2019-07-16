5dfa79ed1aea2a11112a4320618e78c752520f46
Andrew Drozdov <andrew@mrdrozdov.com>
2019-07-16 01:06:45 -0700

    Self attention.

--- train.py
+++ train.py
@@ -35,8 +35,9 @@ chart_utils = ChartUtils()
 
 
 class ModelContainer(nn.Module):
-    def __init__(self, model, loss_func):
+    def __init__(self, embed, model, loss_func):
         super(ModelContainer, self).__init__()
+        self.embed = embed
         self.model = model
         self.loss_func = loss_func
 
@@ -58,12 +59,36 @@ class ConstituentLoss(nn.Module):
         return loss
 
 
-class ChartEncoder(nn.Module):
-    def __init__(self, vocab_size):
-        super(ChartEncoder, self).__init__()
+class SequenceEncoder(nn.Module):
+    def __init__(self, vocab_size, self_attention=False):
+        super(SequenceEncoder, self).__init__()
+        self.self_attention = self_attention
         self.hidden_size = 100
         self.embed = nn.Embedding(vocab_size, self.hidden_size)
-        self.compose = nn.Linear(2*self.hidden_size, self.hidden_size)
+
+        if self.self_attention:
+            self.atten_q = nn.Linear(self.hidden_size, self.hidden_size)
+
+    def run_attention(self, h):
+        q, k, v = h, h, h
+        scores = torch.matmul(self.atten_q(q), k.transpose(1, 2))
+        return torch.sum(scores.unsqueeze(3) * v.unsqueeze(2), 2)
+
+    def forward(self, x):
+        h = self.embed(x)
+        if self.self_attention:
+            h = self.run_attention(h)
+        return h
+
+
+class ChartEncoder(nn.Module):
+    def __init__(self):
+        super(ChartEncoder, self).__init__()
+        self.hidden_size = 100
+        self.compose = nn.Sequential(
+            nn.Linear(2*self.hidden_size, self.hidden_size),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size, self.hidden_size))
         self.score = nn.Linear(2*self.hidden_size, 1) # TODO: Use dot product instead.
 
     def step(self, level):
@@ -120,9 +145,8 @@ class ChartEncoder(nn.Module):
         
     def forward(self, x):
         self.init_batch(x)
-        e = self.embed(x)
-        self.build_chart(e)
-        return e
+        self.build_chart(x)
+        return None
 
 
 class BatchManager(object):
@@ -161,9 +185,10 @@ def main(options):
     tr_reader = JSONLReader(options.tr_file)
     tr_dataset = DatasetReader(tr_reader, config={'max_len': options.tr_max_len}).build()
     batch_iterator = make_batch_iterator(None, tr_dataset)
-    model = ChartEncoder(vocab_size=len(tr_dataset['metadata']['word2idx']))
+    embed = SequenceEncoder(self_attention=options.self_attention, vocab_size=len(tr_dataset['metadata']['word2idx']))
+    model = ChartEncoder()
     loss_func = ConstituentLoss(vocab_size=len(tr_dataset['metadata']['label2idx']))
-    container = ModelContainer(model, loss_func)
+    container = ModelContainer(embed, model, loss_func)
     params = container.parameters()
     optimizer = optim.Adam(params, lr=0.002, betas=(0.9, 0.999), eps=1e-8)
 
@@ -174,9 +199,11 @@ def main(options):
 
     for epoch in range(options.max_epochs):
         for batch_map in batch_iterator.get_iterator():
-            x = BatchManager().prepare_batch(batch_map)
+            seq = BatchManager().prepare_batch(batch_map)
+            seqh = embed(seq)
+            _ = model(seqh)
+
             label_batch = BatchManager().prepare_labels(batch_map)
-            out = model(x)
             loss = loss_func(model.chart, label_batch)
 
             optimizer.zero_grad()
@@ -193,6 +220,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--tr_file', default=os.path.expanduser('~/data/ptb/valid.jsonl'), type=str)
     parser.add_argument('--tr_max_len', default=10, type=int)
+    parser.add_argument('--self_attention', action='store_true')
     parser.add_argument('--max_epochs', default=1000, type=int)
     options = parser.parse_args()
 
